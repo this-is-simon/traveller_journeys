c8f0e8fd8aced3f73221ba7b2ac9e266e79a730e
Simon Atkins <this-is-simon@github.com>
2018-05-16 16:26:22 +0100

    Added first test

--- models/traveller.js
+++ models/traveller.js
@@ -3,8 +3,32 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  const startLocations = this.journeys.map((singleJourney) => {
+    return singleJourney.startLocation;
+  });
+  return startLocations;
 };
+
+Traveller.prototype.getJourneyEndLocations = function() {
+  const endLocations = this.journeys.map((singleJourney) => {
+    return singleJourney.endLocation;
+  });
+  return endLocations;
+}
+
+// Cinema.prototype.findFilmsByGenre = function(genre){
+//   const foundFilms = this.films.filter((singleFilm) => {
+//     return singleFilm.genre === genre;
+//   })
+//   return foundFilms;
+// }
+
+// Cinema.prototype.getListOfTitles = function(){
+//   const listOfTitles = this.films.map((singleFilm) => {
+//     return singleFilm.title;
+//   });
+//   return listOfTitles;
+// }
 
 Traveller.prototype.getJourneyEndLocations = function () {
 
@@ -27,7 +51,7 @@ Traveller.prototype.calculateTotalDistanceTravelled = function () {
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-  
+
 };
 
 
