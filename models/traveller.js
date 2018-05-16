5b15753ff839553207cfcd793f389cdd1724efaa
Simon Atkins <this-is-simon@github.com>
2018-05-16 16:16:22 +0100

    start point

---
+++ models/traveller.js
@@ -0,0 +1,34 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+
+};
+
+Traveller.prototype.getModesOfTransport = function () {
+
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  
+};
+
+
+module.exports = Traveller;
